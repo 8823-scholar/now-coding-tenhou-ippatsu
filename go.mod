89912e56ecb5ddf2700cd44fb7034b8f85fb687a
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2020-06-07 02:59:37 +0900

    get file list

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/8823-scholar/now-coding-tenhou-ippatsu
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	names := get_log_list()
+	fmt.Println(names)
+}
+
+func get_log_list() []string {
+	res, err := http.Get("https://tenhou.net/sc/raw/list.cgi")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := regexp.MustCompile(`\w+\.log\.gz`)
+	names := []string{}
+	for _, matches := range r.FindAllStringSubmatch(string(body), -1) {
+		names = append(names, matches[0])
+	}
+
+	return names
+}
